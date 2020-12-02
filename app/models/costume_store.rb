e6eba23bb8ce56a5b31bce7b3a4ae9b1a79df8a2
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-12-02 16:43:08 +0000

    done

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,14 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+end 
+
+
+
+# Costume
+#   has a name (FAILED - 1)
+#   has a price (FAILED - 2)
+#   has an image url (FAILED - 3)
+#   has a size (FAILED - 4)
+#   knows when it was created (FAILED - 5)
+#   knows when it was last updated (FAILED - 6)
+
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,13 @@
 # Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Base
+end 
+
+# HauntedHouse
+#   has a name (FAILED - 14)
+#   has a location (FAILED - 15)
+#   has a theme (FAILED - 16)
+#   has a price (FAILED - 17)
+#   knows if it's family friendly (FAILED - 18)
+#   has an opening date (FAILED - 19)
+#   has a closing date (FAILED - 20)
+#   has a long, long description (FAILED - 21)
